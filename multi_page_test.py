bf979063a7f924c0ed1cedd4f8ac2441d0f9e4a8
Archie <archiecasey8@gmail.com>
2019-12-08 13:29:01 +0000

    renamed pages

--- multi_page_test.py
+++ multi_page_test.py
@@ -1,31 +1,28 @@
-#to input other code 
-import tkinter as tk                # python 3
-from tkinter import font  as tkfont # python 3
+# Import Packages
+import tkinter as tk
+from tkinter import font as tkfont
 
-class SampleApp(tk.Tk):
+class MainApp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
+        '''Initialise the main app'''
         tk.Tk.__init__(self, *args, **kwargs)
 
         self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
 
-        # the container is where we'll stack a bunch of frames
-        # on top of each other, then the one we want visible
-        # will be raised above the others
+        # Here we will build a container but only make the selected one visible
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        for F in (StartPage, PageOne, PageTwo):
+        for F in (StartPage, CVUploader, CallBooker):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
 
-            # put all of the pages in the same location;
-            # the one on the top of the stacking order
-            # will be the one that is visible.
+            # Put all of the pages in the same location
             frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame("StartPage")
@@ -41,41 +38,46 @@ class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(self, text="This is the start page", font=controller.title_font)
+        label = tk.Label(self, text="Recruitment Platform", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
 
-        button1 = tk.Button(self, text="Go to Page One",
-                            command=lambda: controller.show_frame("PageOne"))
-        button2 = tk.Button(self, text="Go to Page Two",
-                            command=lambda: controller.show_frame("PageTwo"))
+        button1 = tk.Button(self, text="Upload Your CV",
+                            command=lambda: controller.show_frame("CVUploader"))
+        button2 = tk.Button(self, text="Book In A Call",
+                            command=lambda: controller.show_frame("CallBooker"))
         button1.pack()
         button2.pack()
 
 
-class PageOne(tk.Frame):
+class CVUploader(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(self, text="This is page 1", font=controller.title_font)
+        label = tk.Label(self, text="CV Uploader", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
+        button = tk.Button(self, text="Back to the start page",
                            command=lambda: controller.show_frame("StartPage"))
         button.pack()
 
+    def UploadAction(event=None):
+        """Create an upload button"""
+        filename = filedialog.askopenfilename()
+        print('Selected:', filename)
 
-class PageTwo(tk.Frame):
+class CallBooker(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(self, text="This is page 2", font=controller.title_font)
+        label = tk.Label(self, text="Call Booker", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
+        button = tk.Button(self, text="Back to the start page",
                            command=lambda: controller.show_frame("StartPage"))
         button.pack()
 
 
 if __name__ == "__main__":
-    app = SampleApp()
+    app = MainApp()
+    app.geometry("700x800+10+10")
     app.mainloop()
