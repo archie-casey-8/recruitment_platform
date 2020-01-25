746431da20a90a7a9166945a6a58b8bab8db50cc
Archie <archiecasey8@gmail.com>
2020-01-25 16:41:17 +0000

    created multipage app in dash

---
+++ importing_word_data.py
@@ -0,0 +1,33 @@
+# Code to read in a word file
+
+from xml.etree.cElementTree import XML
+import zipfile
+
+WORD_NAMESPACE = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
+PARA = WORD_NAMESPACE + 'p'
+TEXT = WORD_NAMESPACE + 't'
+
+def get_docx_text(path):
+    """
+    Take the path of a docx file as argument, return the text in unicode.
+    """
+    document = zipfile.ZipFile(path)
+    xml_content = document.read('word/document.xml')
+    document.close()
+    tree = XML(xml_content)
+
+    paragraphs = []
+    for paragraph in tree.getiterator(PARA):
+        texts = [node.text
+                 for node in paragraph.getiterator(TEXT)
+                 if node.text]
+        if texts:
+            paragraphs.append(''.join(texts))
+
+    return '\n\n'.join(paragraphs)
+
+tests = get_docx_text("test_data/example_cv.docx")
+
+# Code to write out to a text file
+f = open("example_csv.txt","w+")
+f.write(tests)
---
+++ index.py
@@ -0,0 +1,33 @@
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+
+from app import app
+from apps import cv_uploader, call_booker
+
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
+
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/':
+        return html.Div([
+            dcc.Link('Go to CV uploader', href='/cv_uploader'),
+            html.Br(),
+            dcc.Link('Go to Call Booker', href='/apps/call_booker')
+        ])
+
+    if pathname == '/cv_uploader':
+         return cv_uploader.layout
+    elif pathname == '/apps/call_booker':
+         return call_booker.layout
+    else:
+        return '404'
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
