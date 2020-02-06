88cb289a0a156c68b841eadf88de79ff2ccf1aba
Archie <archiecasey8@gmail.com>
2020-02-06 20:18:55 +0000

    All personal info working

--- apps/cv_uploader.py
+++ apps/cv_uploader.py
@@ -18,7 +18,21 @@ layout = html.Div(
             id="second_name_input",
             type="text",
             placeholder="Enter Second Name",
-        ) ,
+        ),
+
+         html.P("Mobile Number:"),
+         dcc.Input(
+             id="phone_number_input",
+             type="number",
+             placeholder="Enter Mobile Number",
+         ),
+         html.P("Email Address:"),
+         dcc.Input(
+             id="email_address_input",
+             type="text",
+             placeholder="Enter Email Address",
+         ),
+
         html.Button('Submit', id='button'),
 
     ]
@@ -30,16 +44,18 @@ layout = html.Div(
     Output("out-all-types", "children"),
     [Input("first_name_input", "value"),
      Input("second_name_input", "value"),
+     Input("phone_number_input", "value"),
+     Input("email_address_input", "value"),
      Input('button', 'n_clicks')],
 )
-def save_details(first_name_input, second_name_input, n_clicks):
+def save_details(first_name_input, second_name_input, phone_number_input,  email_address_input, n_clicks):
 
     if n_clicks is None:
         return None
     else:
         with open('database.csv', mode='a') as database_file:
             database_file = csv.writer(database_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            database_file.writerow([first_name_input, second_name_input])
+            database_file.writerow([first_name_input, second_name_input, phone_number_input, email_address_input])
 
 
 def update_output(n_clicks, value):
