45be246abb07a2268788cc10a39f6f15db28e95f
Archie <archiecasey8@gmail.com>
2020-03-28 12:18:21 +0000

    unit tests added

--- apps/call_booker.py
+++ apps/call_booker.py
@@ -4,6 +4,7 @@ import csv
 from datetime import datetime as dt
 from app import app
 from dash.dependencies import Input, Output
+from call_booker_functions import date_time_dict_constructor
 
 layout = html.Div(
     [
@@ -39,16 +40,24 @@ layout = html.Div(
          dcc.DatePickerSingle(
          id='my_date_picker_single',
          min_date_allowed=dt(1995, 8, 5),
-         max_date_allowed=dt(2017, 9, 19),
+         max_date_allowed=dt(2023, 9, 19),
          initial_visible_month=dt(2017, 8, 5),
          date=str(dt(2017, 8, 25, 23, 59, 59))
          ),
          html.Button('Submit', id='button'),
+
+         html.P("Select time:"),
+         dcc.Dropdown(
+            options=[
+                {'label': 'New York City', 'value': 'NYC'},
+                {'label': 'Montreal', 'value': 'MTL'},
+                {'label': 'San Francisco', 'value': 'SF'}
+            ],
+            searchable=False
+        )
     ]
     + [html.Div(id="out-all-types-call-booker")]
 )
-
-
 
 @app.callback(
     Output("out-all-types-call-booker", "children"),
@@ -66,11 +75,9 @@ def save_details(first_name_input, second_name_input, phone_number_input, email_
         return None
     else:
 
-
         with open('database/call_booker_table.csv', mode='a') as database_file:
             database_file = csv.writer(database_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             database_file.writerow([first_name_input, second_name_input, phone_number_input, email_address_input, my_date_picker_single])
-
 
 def parse_datetime(value, date):
     string_prefix = 'You have selected: '
@@ -78,6 +85,10 @@ def parse_datetime(value, date):
         date = dt.strptime(date.split(' ')[0], '%Y-%m-%d')
         date_string = date.strftime('%B %d, %Y')
         return string_prefix + date_string
+
+def get_available_times(my_date_picker_single):
+    date_time_dict = date_time_dict_constructor(30)
+    print(date_time_dict[my_date_picker_single])
 
 
 def update_output(n_clicks, value):
